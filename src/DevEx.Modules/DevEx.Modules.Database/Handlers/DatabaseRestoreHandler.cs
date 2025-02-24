21b1acc05016e0cf1038dca2ce10dd84f66ca619
leandromonaco <leandro.monaco@gmail.com>
2025-02-24 14:01:30 +1000

    added "database restore" command

---
+++ src/DevEx.Modules/DevEx.Modules.Database/Handlers/DatabaseRestoreHandler.cs
@@ -0,0 +1,37 @@
+﻿using DevEx.Core;
+using DevEx.Core.Helpers;
+using DevEx.Core.Storage;
+using DevEx.Modules.Database.Helpers;
+
+namespace DevEx.Modules.Database.Handlers
+{
+    public class DatabaseRestoreHandler : ICommandHandler
+    {
+        public void Execute(Dictionary<string, string> options)
+        {
+            var containerName = UserStorageManager.GetDecryptedValue("SqlContainerName");
+            var backupLocation = UserStorageManager.GetDecryptedValue("SqlBackupLocation");
+            var backupFilename = new FileInfo(backupLocation);
+            var masterConnectionString = UserStorageManager.GetDecryptedValue("SqlMasterConnectionString");
+            var databaseName = UserStorageManager.GetDecryptedValue("SqlDatabaseName");
+
+            TerminalHelper.Run(TerminalHelper.ConsoleMode.Powershell, $"docker cp {backupLocation} {containerName}:/var/opt/mssql/backup/");
+
+            TerminalHelper.Run(TerminalHelper.ConsoleMode.Powershell, $"docker restart {containerName}");
+
+            // Wait for 30 seconds
+            Thread.Sleep(30000);
+
+            var command = @$"RESTORE DATABASE {databaseName} 
+                             FROM DISK = N'/var/opt/mssql/backup/{backupFilename.Name}' 
+                             WITH MOVE 'DevExLead' TO '/var/opt/mssql/data/{databaseName}.mdf', 
+                                  MOVE 'DevExLead_log' TO '/var/opt/mssql/data/{databaseName}.ldf', 
+                             NOUNLOAD, 
+                             REPLACE, 
+                             RECOVERY;
+                            ";
+            
+            DatabaseHelper.RunSqlCommand(masterConnectionString, command);
+        }
+    }
+}
---
+++ src/DevEx.Modules/DevEx.Modules.Database/Helpers/DatabaseHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using Spectre.Console;
+
+namespace DevEx.Modules.Database.Helpers
+{
+    public class DatabaseHelper
+    {
+        public static void RunSqlCommand(string connectionString, string sql, List<SqlParameter> parameters = null)
+        {
+            AnsiConsole.MarkupLine($"[yellow]{sql}[/]");
+
+            var sqlConnection = new SqlConnection(connectionString);
+
+            using (sqlConnection)
+            {
+                sqlConnection.Open();
+
+                var sqlCommand = new SqlCommand(sql, sqlConnection);
+                if (parameters != null)
+                {
+                    sqlCommand.Parameters.AddRange(parameters.ToArray());
+                }
+                sqlCommand.ExecuteNonQuery();
+
+                sqlConnection.Close();
+            }
+        }
+    }
+}
+
