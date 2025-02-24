1a22e4ac4abf9a22eaee031c7414b546e4fd28c9
leandromonaco <leandro.monaco@gmail.com>
2025-02-24 14:45:03 +1000

    added "database upgrade" and "database dacpac" commands

---
+++ src/DevEx.Modules/DevEx.Modules.Database/Handlers/DatabaseUpgradeHandler.cs
@@ -0,0 +1,23 @@
+﻿using DevEx.Core;
+using DevEx.Core.Helpers;
+using DevEx.Core.Storage;
+using Spectre.Console;
+
+namespace DevEx.Modules.Database.Handlers
+{
+    public class DatabaseUpgradeHandler : ICommandHandler
+    {
+        public void Execute(Dictionary<string, string> options)
+        {
+            var databaseName = UserStorageManager.GetDecryptedValue("SqlDatabaseName");
+            var connectionString = UserStorageManager.GetDecryptedValue("SqlConnectionString")
+                                                     .Replace("{{InitialCatalog}}", databaseName);
+            var dacpacLocation = UserStorageManager.GetDecryptedValue("SqlDacpacLocation");
+
+            var command = @$"SqlPackage /Action:Publish /SourceFile:'{dacpacLocation}' /TargetConnectionString:'{connectionString}'";
+            TerminalHelper.Run(TerminalHelper.ConsoleMode.Powershell, command);
+
+            AnsiConsole.MarkupLine($"[green]Database Schema has been updated.[/]");
+        }
+    }
+}
