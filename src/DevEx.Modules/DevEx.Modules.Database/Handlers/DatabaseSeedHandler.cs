087c4caf18920bddddc2e39365da978ae7f1b29e
leandromonaco <leandro.monaco@gmail.com>
2025-02-25 11:05:52 +1000

    Added "database seed" command

---
+++ src/DevEx.Modules/DevEx.Modules.Database/Handlers/DatabaseSeedHandler.cs
@@ -0,0 +1,21 @@
+﻿using DevEx.Core;
+using DevEx.Core.Helpers;
+using DevEx.Core.Storage;
+using Spectre.Console;
+
+namespace DevEx.Modules.Database.Handlers
+{
+    public class DatabaseSeedHandler : ICommandHandler
+    {
+        public void Execute(Dictionary<string, string> options)
+        {
+            var databaseName = UserStorageManager.GetDecryptedValue("SqlDatabaseName");
+            var connectionString = UserStorageManager.GetDecryptedValue("SqlConnectionString")
+                                                     .Replace("{{InitialCatalog}}", databaseName);
+            var sqlDatabaseSeedCommand = UserStorageManager.GetDecryptedValue("SqlDatabaseSeedCommand");
+            var sqlDatabaseSeedPath = UserStorageManager.GetDecryptedValue("SqlDatabaseSeedPath");
+            TerminalHelper.Run(TerminalHelper.ConsoleMode.Powershell, sqlDatabaseSeedCommand, sqlDatabaseSeedPath);
+            AnsiConsole.MarkupLine($"[green]{databaseName} has been seeded.[/]");
+        }
+    }
+}
