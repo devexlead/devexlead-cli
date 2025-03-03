8ba437811d3c666bfe095ca83ee2d1390964c977
leandromonaco <leandro.monaco@gmail.com>
2025-03-04 09:36:51 +1000

    added list commands for vault and command

---
+++ src/DevEx.Modules/DevEx.Modules.Command/Handlers/CommandListHandler.cs
@@ -0,0 +1,27 @@
+﻿using DevEx.Core;
+using DevEx.Core.Storage;
+using Spectre.Console;
+
+namespace DevEx.Modules.Command.Handlers
+{
+    internal class CommandListHandler : ICommandHandler
+    {
+        public void Execute(Dictionary<string, string> options)
+        {
+            var userStorage = UserStorageManager.GetUserStorage();
+            var commands = userStorage.Commands;
+
+            var table = new Table();
+            table.AddColumn("Name");
+            table.AddColumn("Body");
+            table.AddColumn("Path");
+
+            foreach (var command in commands)
+            {
+                table.AddRow(command.Name, command.Body, command.Path);
+            }
+
+            AnsiConsole.Write(table);
+        }
+    }
+}
---
+++ src/DevEx.Modules/DevEx.Modules.Vault/Handlers/Vault/WindowsVaultListCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using DevEx.Core;
+using DevEx.Core.Helpers;
+using DevEx.Core.Storage;
+using Spectre.Console;
+
+namespace DevEx.Modules.Configuration.Handlers.Vault
+{
+    public class WindowsVaultListCommandHandler : ICommandHandler
+    {
+        public void Execute(Dictionary<string, string> options)
+        {
+            var userStorage = UserStorageManager.GetUserStorage();
+            var vaultItems = userStorage.Vault;
+
+            var table = new Table();
+            table.AddColumn("Name");
+            table.AddColumn("Decrypted Value");
+            table.AddColumn("Encrypted Value");
+
+            foreach (var item in vaultItems)
+            {
+                table.AddRow(item.Key, EncryptionHelper.Decrypt(item.Value), item.Value);
+            }
+
+            AnsiConsole.Write(table);
+        }
+    }
+}
