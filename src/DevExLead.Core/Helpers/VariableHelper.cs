5170c6b19f96a905423a93c1134d1d7436243701
leandromonaco <leandro.monaco@gmail.com>
2025-03-17 19:03:52 +1000

    Fixed "Not specifying a timeout for regular expressions is security-sensitive"

--- src/DevExLead.Core/Helpers/VariableHelper.cs
+++ src/DevExLead.Core/Helpers/VariableHelper.cs
@@ -7,7 +7,12 @@ namespace DevExLead.Core.Helpers
     {
         public static string ReplacePlaceholders(string input)
         {
-            return Regex.Replace(input, @"\{\{([^}]+)\}\}", match =>
+            //Timeout to address: Not specifying a timeout for regular expressions is security-sensitive
+            //https://sonarsource.github.io/rspec/#/rspec/S6444/csharp
+            var timeout = TimeSpan.FromSeconds(5); 
+            var regex = new Regex(@"\{\{([^}]+)\}\}", RegexOptions.None, timeout);
+
+            return regex.Replace(input, match =>
             {
                 var key = match.Groups[1].Value;
                 var placeHolder = UserStorageManager.GetDecryptedValue(key) ?? match.Value;
