0a0d0ee7b553a8a880b56698a04d8e4c7b02d270
Leo <leandromonaco@users.noreply.github.com>
2025-07-18 13:30:03 +1000

    jira watch command improvements to spot sprint changes

---
+++ src/DevExLead.Core/Helpers/ParameterHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace DevExLead.Core.Helpers
+{
+    public class ParameterHelper
+    {
+        public static bool ReadBoolParameter(Dictionary<string, string> options, string paramName)
+        {
+            bool param = false;
+
+            if (options.ContainsKey(paramName) && !string.IsNullOrEmpty(options[paramName]))
+            {
+                param = bool.Parse(options[paramName]);
+            }
+
+            return param;
+        }
+
+        public static string ReadStringParameter(Dictionary<string, string> options, string paramName)
+        {
+            if (options.ContainsKey(paramName) && !string.IsNullOrEmpty(options[paramName]))
+            {
+                return options[paramName];
+            }
+            return null;
+        }
+
+    }
+}
--- src/DevExLead.Modules/DevExLead.Modules.Jira/Handlers/JiraWatchHandler.cs
+++ src/DevExLead.Modules/DevExLead.Modules.Jira/Handlers/JiraWatchHandler.cs
@@ -1,6 +1,9 @@
-﻿using DevExLead.Core;
+﻿using System.Text.Json;
+using DevExLead.Core;
+using DevExLead.Core.Helpers;
 using DevExLead.Core.Storage;
 using DevExLead.Integrations.JIRA;
+using DevExLead.Integrations.JIRA.Model;
 using Spectre.Console;
 
 namespace DevExLead.Modules.Jira.Handlers
@@ -11,14 +14,8 @@ namespace DevExLead.Modules.Jira.Handlers
         {
             try
             {
-                bool isVerbose = false;
-                if (options.TryGetValue("isVerbose", out var isVerboseString))
-                {
-                    if (!bool.TryParse(isVerboseString, out isVerbose))
-                    {
-                        throw new ArgumentException("Invalid value for isVerbose. It must be a boolean.");
-                    }
-                }
+                bool isVerbose = ParameterHelper.ReadBoolParameter(options, "isVerbose");
+                bool isSnapshot = ParameterHelper.ReadBoolParameter(options, "isSnapshot");
 
                 var atlassianBaseUrl = UserStorageManager.GetDecryptedValue("Atlassian:BaseUrl");
                 if (atlassianBaseUrl == null) return;
@@ -31,18 +28,30 @@ namespace DevExLead.Modules.Jira.Handlers
 
                 var jiraConnector = new JiraConnector(atlassianBaseUrl, atlassianUser, atlassianKey, isVerbose);
 
-                var jiraWatchJql = UserStorageManager.GetDecryptedValue("Jira:WatchJql");
-                var jiraIssues = jiraConnector.RunJqlAsync(jiraWatchJql).Result;
+                var jiraBoardId = int.Parse(UserStorageManager.GetDecryptedValue("Atlassian:BoardId"));
+                var jiraSprints = jiraConnector.FetchSprints(jiraBoardId).Result;
+
+                var currentSprint = jiraSprints.FirstOrDefault(s => s.State == "active");
+                var jiraWatchJql = $"sprint = {currentSprint?.Id}";
                 AnsiConsole.MarkupLine($"[green]Query: {jiraWatchJql} [/]");
+                var jiraIssues = jiraConnector.RunJqlAsync(jiraWatchJql).Result;
 
-                var jiraWatchUserEmail = UserStorageManager.GetDecryptedValue("Jira:WatchUserEmail");
-
-                foreach (var jiraIssue in jiraIssues)
+                var filePath = Path.Combine(Environment.CurrentDirectory, "jira-issues.json");
+                if (isSnapshot)
                 {
-                    AnsiConsole.MarkupLine($"[green]Watching: {atlassianBaseUrl}/browse/{jiraIssue.Key} [/]");
-                    await jiraConnector.WatchIssueAsync(jiraIssue.Key, jiraWatchUserEmail);
+                    SaveJsonFile(jiraIssues, filePath);
+                }
+                else
+                {
+                    var json = File.ReadAllText(filePath);
+                    var plannedIssues = JsonSerializer.Deserialize<List<JiraIssue>>(json);
+
+                    FindAddedIssues(jiraIssues, plannedIssues);
+                    FindDeletedIssues(jiraIssues, plannedIssues);
+                    FindReestimatedIssues(jiraIssues, plannedIssues);
                 }
 
+                await WatchIssues(atlassianBaseUrl, jiraConnector, jiraIssues);
             }
             catch (Exception ex)
             {
@@ -50,5 +59,75 @@ namespace DevExLead.Modules.Jira.Handlers
             }
         }
 
+        private static async Task WatchIssues(string atlassianBaseUrl, JiraConnector jiraConnector, List<JiraIssue> jiraIssues)
+        {
+            var jiraWatchUserEmail = UserStorageManager.GetDecryptedValue("Jira:WatchUserEmail");
+
+            foreach (var jiraIssue in jiraIssues)
+            {
+                AnsiConsole.MarkupLine($"[green]Watching: {atlassianBaseUrl}/browse/{jiraIssue.Key} ({jiraIssue.Fields.Summary}) [/]");
+                await jiraConnector.WatchIssueAsync(jiraIssue.Key, jiraWatchUserEmail);
+            }
+        }
+
+        private static void FindDeletedIssues(List<JiraIssue> jiraIssues, List<JiraIssue>? plannedIssues)
+        {
+            var deletedIssues = plannedIssues == null
+                                    ? new List<JiraIssue>()
+                                    : plannedIssues.Where(p => !jiraIssues.Any(j => j.Key == p.Key)).ToList();
+
+            AnsiConsole.MarkupLine($"[yellow]Deleted Issues: {deletedIssues.Count}[/]");
+            foreach (var issue in deletedIssues)
+            {
+                AnsiConsole.MarkupLine($"[red]- {issue.Key}: {issue.Fields.Summary}[/]");
+            }
+        }
+
+        private static void FindAddedIssues(List<JiraIssue> jiraIssues, List<JiraIssue>? plannedIssues)
+        {
+            var addedIssues = jiraIssues
+                                    .Where(j => plannedIssues == null || !plannedIssues.Any(p => p.Key == j.Key))
+                                    .ToList();
+
+            // Output results
+            AnsiConsole.MarkupLine($"[yellow]Added Issues: {addedIssues.Count}[/]");
+            foreach (var issue in addedIssues)
+            {
+                AnsiConsole.MarkupLine($"[green]+ {issue.Key}: {issue.Fields.Summary}[/]");
+            }
+        }
+
+        private static void FindReestimatedIssues(List<JiraIssue> jiraIssues, List<JiraIssue>? plannedIssues)
+        {
+            if (plannedIssues == null) return;
+
+            var reestimatedIssues = jiraIssues
+                .Join(plannedIssues,
+                      current => current.Key,
+                      planned => planned.Key,
+                      (current, planned) => new
+                      {
+                          Key = current.Key,
+                          Summary = current.Fields.Summary,
+                          OldEstimate = planned.Fields.Points,
+                          NewEstimate = current.Fields.Points
+                      })
+                .Where(x => x.OldEstimate != x.NewEstimate)
+                .ToList();
+
+            AnsiConsole.MarkupLine($"[yellow]Re-estimated Issues: {reestimatedIssues.Count}[/]");
+            foreach (var issue in reestimatedIssues)
+            {
+                AnsiConsole.MarkupLine(
+                    $"[blue]* {issue.Key}: {issue.Summary}[/] [grey](Estimate changed from {issue.OldEstimate ?? 0} to {issue.NewEstimate ?? 0})[/]");
+            }
+        }
+
+        private static void SaveJsonFile(List<JiraIssue> jiraIssues, string filePath)
+        {
+            var json = JsonSerializer.Serialize(jiraIssues, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, json);
+            AnsiConsole.MarkupLine($"[yellow]Saved {jiraIssues.Count} issues to {filePath}[/]");
+        }
     }
 }
